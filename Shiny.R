b29fc52b929145cdbe6b1a9df047d6ace8de2463
lisamarcink <lisa.marcinkiewicz@gmail.com>
2021-01-05 13:29:18 -0600

    Updated app output to display Plot in Plotly and Data Table.
    Also, added ability for user to select theme.

--- Shiny.R
+++ Shiny.R
@@ -5,38 +5,54 @@ library(tm)
 library(stringr)
 library(shiny)
 library(ggthemes)
+library(plotly)
+library(data.table)
+library(shinythemes)
 
 ui <- fluidPage(
   titlePanel("Nobel Prize"),
-  selectInput(inputId = "category", label = "Select a Category", choices = c("chemistry", "economics", "literature", "medicine", "peace", "physics")),
+  themeSelector(),
+  sidebarLayout(
+    sidebarPanel(selectInput(inputId = "category", label = "Select a Category", choices = c("chemistry", "economics", "literature", "medicine", "peace", "physics")),
   sliderInput("year", "Select Year", min = 1900, max = 2010, value = 1970),
- 
-   # Add plot output to display awards by prize category
-  plotOutput('plot_awards')
+    ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel('Table',
+    #Add DT output to display awards by prize category
+    DT::DTOutput('table_awards_by_category')),
+    tabPanel('Plot',
+    # Add plotly output to display awards by prize category
+    plotly::plotlyOutput('plotly_awards_by_category'))
   )
+  )
+  )
+ )
 
 server <- function(input, output, session){
+  laureateURL <- "http://api.nobelprize.org/v1/laureate.csv"
+  laureates <- read.csv(laureateURL)
   
-  
-  output$plot_awards <- renderPlot({
-    
-    laureateURL <- "http://api.nobelprize.org/v1/laureate.csv"
-    laureates <- read.csv(laureateURL)
-    
-    #number of awards by category by year
-    
+  #Function that returns number of awards by category by year
+  awards_by_category <- function(){
     awards_by_category <- laureates %>%
-      filter(category == input$category) %>%
-      group_by(year, gender) %>%
-      count()
+    filter(category == input$category) %>%
+    group_by(year, gender) %>%
+    count()
+  }
 
-    ggplot(awards_by_category, aes(x = year, y=n)) +
-      geom_col(aes(fill = as.factor(gender))) +
-      labs(title="Prizes Awarded by Category",
-           x ="Year", y = "Number of Prizes") +
-      theme_classic()
-      
-    
+  #Render a data table output named "table_awards_by_category"
+   output$table_awards_by_category <- DT::renderDT({
+    DT::datatable(awards_by_category())
+  })
+  
+  #Render a plotly output named "plot_awards_by_category"
+    output$plotly_awards_by_category <- plotly::renderPlotly({
+      ggplot(awards_by_category(), aes(x = year, y=n)) +
+        geom_col(aes(fill = as.factor(gender))) +
+        labs(title="Prizes Awarded by Category",
+             x ="Year", y = "Number of Prizes") +
+        theme_classic()
   })
     
 }
